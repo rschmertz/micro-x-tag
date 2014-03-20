4879ba15a2ae69cb45764e619be495395c504b4a
Bob Schmertz <rschmertz@42six.com>
2014-03-19 22:44:59 -0400

    Extract fragment from template, execute dynamically loaded JavaScript using fragment

---
+++ public/components/hello/hello.js
@@ -0,0 +1,3 @@
+console.log("Executing dynamic JavaScript");
+document.body.appendChild(microXTag.getFragmentFromTemplate('hello-tmpl'));
+//@ sourceURL=hello.js
--- public/js/micro-x-tag.js
+++ public/js/micro-x-tag.js
@@ -3,13 +3,25 @@ microXTag = (function ($) {
         $.each(importList, function (index, importFile) {
             $.get(importFile)
                 .done(function (html) {
-                    var elements = $(html);
-                    $('body').append(elements);
+                    var $elements = $(html);
+                    $('body').append($elements);
                 })
         });
     };
 
+    function getFragmentFromTemplate(templateID) {
+        var text = $("#" + templateID).text();
+        var div = document.createElement('div');
+        div.innerHTML = text;
+        var df = document.createDocumentFragment();
+        while (div.firstChild) {
+            df.appendChild(div.firstChild);
+        }
+        return df;
+    };
+
     return {
-        loadImports: loadImports
+        loadImports: loadImports,
+        getTemplate: getTemplate
     }
 })(jQuery);
